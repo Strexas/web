9649eb03edb63de9bbed4bd77573d1c1635248cc
Strexas <kirsnauskas@bk.ru>
2021-05-20 01:36:50 +0300

    heroku

--- main.py
+++ main.py
@@ -54,4 +54,5 @@ if __name__ == '__main__':
 
     # пользователь
     app.register_blueprint(get_user_blueprint)
-    app.run(host='127.0.0.1', port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='127.0.0.1', port=port)
